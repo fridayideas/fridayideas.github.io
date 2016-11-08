dfbb91285092ad9a3aed40384ab8eb5deac14459
Ryan Samarajeewa <ryan.samarajeewa@gmail.com>
2016-11-07 20:39:03 -0800

    Relocate resources to /css

--- gulpfile.js
+++ gulpfile.js
@@ -53,13 +53,13 @@ gulp.task('minify-js', function() {
 // Copy vendor libraries from /node_modules into /vendor
 gulp.task('copy', function() {
     gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('vendor/bootstrap'))
+        .pipe(gulp.dest('css/bootstrap'))
 
     gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('vendor/jquery'))
+        .pipe(gulp.dest('css/jquery'))
 
     gulp.src(['node_modules/simple-line-icons/*/*'])
-        .pipe(gulp.dest('vendor/simple-line-icons'))
+        .pipe(gulp.dest('css/simple-line-icons'))
 
 
     gulp.src([
@@ -70,7 +70,7 @@ gulp.task('copy', function() {
             '!node_modules/font-awesome/*.md',
             '!node_modules/font-awesome/*.json'
         ])
-        .pipe(gulp.dest('vendor/font-awesome'))
+        .pipe(gulp.dest('css/font-awesome'))
 })
 
 // Run everything
